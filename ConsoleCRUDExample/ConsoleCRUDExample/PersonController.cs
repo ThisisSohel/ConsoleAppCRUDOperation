8db9692e2a4dc054d33b488d304556380cf34adf
Sohel Rana <mdsrana11@gmail.com>
2024-10-07 15:59:08 +0600

    Complete CRUD without Using Layer Architecture

---
+++ ConsoleCRUDExample/ConsoleCRUDExample/PersonController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCRUDExample
+{
+    public class PersonController
+    {
+        private List<Person> _person;
+
+        public PersonController()
+        {
+            _person = new List<Person>();
+        }
+
+        /// <summary>
+        /// Create a new objcet in the database
+        /// </summary>
+        /// <param name="person"></param>
+        public void CreatePerson(Person person)
+        {
+            _person.Add(person);
+            Console.WriteLine("Person is added successfully!\n\n");
+            Console.WriteLine("Person List");
+            Console.WriteLine("......................................");
+            GellAllPerson();
+        }
+
+        /// <summary>
+        /// Retreive all the data from database
+        /// </summary>
+        public void GellAllPerson()
+        {
+            if (_person.Count <= 0)
+            {
+                Console.WriteLine("\n\nNo person is available now! Please add a new person!");
+            }
+            else
+            {
+                foreach (Person person in _person)
+                {
+                    Console.WriteLine($"Id: {person.Id}- Name: {person.Name}- Phone: {person.Phone}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Find the individual data from database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Person GetById(int? id)
+        {
+            return _person.FirstOrDefault(x => x.Id == id);
+        }
+
+        /// <summary>
+        /// This method is use to delete an object from the database!
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        public void UpdatePerson(int id, Person model)
+        {
+            var personToUpdate = GetById(id);
+            if (personToUpdate != null)
+            {
+                personToUpdate.Id = model.Id; 
+                personToUpdate.Name = model.Name;
+                personToUpdate.Phone = model.Phone;
+                Console.WriteLine("\nPerson is updated successfully!");
+                Console.WriteLine("Person List");
+                Console.WriteLine("......................................");
+                GellAllPerson();
+            }
+            else
+            {
+                Console.WriteLine("Person is not found to update!");
+            }
+        }
+
+        /// <summary>
+        /// This method is used to delete an object from the database
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeletePerson(int id)
+        {
+            var personToDelete = GetById(id);   
+            if(personToDelete != null)
+            {
+                _person.Remove(personToDelete);
+                Console.WriteLine("\nPerson is Deleted!");
+                Console.WriteLine("Person List");
+                Console.WriteLine("......................................");
+                GellAllPerson();
+            }
+        }
+    }
+}
---
+++ ConsoleCRUDExample/ConsoleCRUDExample/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleCRUDExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonController controller = new PersonController();
+
+            while (true)
+            {
+                Console.WriteLine("\n--- Person Management ---");
+                Console.WriteLine("1. Create Person");
+                Console.WriteLine("2. View All Persons");
+                Console.WriteLine("3. Update Person");
+                Console.WriteLine("4. Delete Person");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+
+                string? option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        {
+                            Person newPerson = new Person();
+                            Console.WriteLine("Enter the person Id:");
+                            newPerson.Id = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the person name:");
+                            newPerson.Name = Console.ReadLine();
+                            Console.WriteLine("Enter the person Phone:");
+                            newPerson.Phone = Console.ReadLine();
+                            controller.CreatePerson(newPerson);
+                            break;
+                        }
+                    case "2":
+                        {
+                            controller.GellAllPerson();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.Write("Enter ID of the person to update: ");
+                            int updateId = int.Parse(Console.ReadLine());
+                            var updatedPerson = new Person();
+                            updatedPerson.Id = updateId;
+                            Console.Write("Enter New Name: ");
+                            updatedPerson.Name = Console.ReadLine();
+                            Console.Write("Enter New Phone: ");
+                            updatedPerson.Phone = Console.ReadLine();
+                            controller.UpdatePerson(updateId, updatedPerson);
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.Write("Enter ID of the person to delete: ");
+                            int deleteId = int.Parse(Console.ReadLine());
+                            controller.DeletePerson(deleteId);
+                            break;
+                        }
+                    case "5":
+                        {
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid option.Please try again.");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
